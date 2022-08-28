194220aa2365c5d7a02bf54ee5c7a063079d2196
Intesar Haque <xyz@gmail.com>
2022-08-28 11:04:34 +0600

    Testing Env

--- src/main/java/com/ds/meeting/auth/controller/UserController.java
+++ src/main/java/com/ds/meeting/auth/controller/UserController.java
@@ -10,30 +10,65 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @CrossOrigin("*")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final AuthenticationManager authenticationManager;
-    private final UserService userService;
+//    private final AuthenticationManager authenticationManager;
+//    private final UserService userService;
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<?> register(@RequestBody Users user){
+//        return userService.createUser(user);
+//    }
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<?> authenticate(@RequestParam String username, @RequestParam String password){
+//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+//        TokenResponseDto responseDto = new TokenResponseDto();
+//        responseDto.setColorProfile("Default");
+//        Users user = (Users) authentication.getPrincipal();
+//        responseDto.setFullName(user.getName());
+//        responseDto.setId(user.getId());
+//        responseDto.setUsername(user.getUsername());
+//        responseDto.setToken(userService.authenticate(username, password));
+//        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+//    }
 
+
+    private final List<Users> USERS = new ArrayList<>();
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Users user){
-        return userService.createUser(user);
+        USERS.add(user);
+        return new ResponseEntity<>("User created", HttpStatus.OK);
     }
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticate(@RequestParam String username, @RequestParam String password){
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        TokenResponseDto responseDto = new TokenResponseDto();
-        responseDto.setColorProfile("Default");
-        Users user = (Users) authentication.getPrincipal();
-        responseDto.setFullName(user.getName());
-        responseDto.setId(user.getId());
-        responseDto.setUsername(user.getUsername());
-        responseDto.setToken(userService.authenticate(username, password));
-        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        int idx = -1;
+        for(int i=0; i < USERS.size(); i++){
+            if(Objects.nonNull(USERS.get(i))
+                    && Objects.equals(USERS.get(i).getUsername(), username)
+                    && Objects.equals(USERS.get(i).getPassword(), password)){
+                idx=i;
+                break;
+            }
+        }
+        if(idx>-1){
+            TokenResponseDto responseDto = new TokenResponseDto();
+            responseDto.setColorProfile("Default");
+            Users user = USERS.get(idx);
+            responseDto.setFullName(user.getName());
+            responseDto.setId(idx+1);
+            responseDto.setUsername(user.getUsername());
+            responseDto.setToken(UUID.randomUUID().toString());
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        } else {
+            throw new UsernameNotFoundException("Invalid Credentials");
+        }
     }
 }
--- src/main/java/com/ds/meeting/meeting/controller/MeetingController.java
+++ src/main/java/com/ds/meeting/meeting/controller/MeetingController.java
@@ -2,32 +2,77 @@ package com.ds.meeting.meeting.controller;
 
 import com.ds.meeting.meeting.service.MeetingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.*;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("meeting")
 @RequiredArgsConstructor
 public class MeetingController {
-    private final MeetingService meetingService;
+//    private final MeetingService meetingService;
+//
+//    @PostMapping("/create")
+//    public ResponseEntity<?> scheduleMeeting(
+//            @RequestParam String meetingCode,
+//            @RequestParam(required = false) Integer groupId,
+//            @RequestParam(required = false) Date scheduleDate
+//    ) { return meetingService.scheduleMeeting(meetingCode, groupId, scheduleDate); }
+//
+//    @PostMapping("/join")
+//    public ResponseEntity<?> joinMeeting( @RequestParam String meetingCode, @RequestParam String peerId, @RequestParam(required = false) String displayName, @RequestParam(required = false) Integer userId) {
+//        return meetingService.joinMeeting(meetingCode, displayName, peerId, userId);
+//    }
+//
+//    @PostMapping("/leave")
+//    public ResponseEntity<?> leaveMeeting( @RequestParam String peerId) {
+//        return meetingService.leaveMeeting(peerId);
+//    }
 
+
+    private final HashMap<String, List<Map<String, Object>>> MEETINGS = new HashMap<>();
     @PostMapping("/create")
     public ResponseEntity<?> scheduleMeeting(
             @RequestParam String meetingCode,
             @RequestParam(required = false) Integer groupId,
             @RequestParam(required = false) Date scheduleDate
-    ) { return meetingService.scheduleMeeting(meetingCode, groupId, scheduleDate); }
+    ) {
+        MEETINGS.put(meetingCode, new ArrayList<>());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @PostMapping("/join")
     public ResponseEntity<?> joinMeeting( @RequestParam String meetingCode, @RequestParam String peerId, @RequestParam(required = false) String displayName, @RequestParam(required = false) Integer userId) {
-        return meetingService.joinMeeting(meetingCode, displayName, peerId, userId);
+        Map<String, Object> map = new HashMap<>();
+        map.put("peerId", peerId);
+        map.put("name", displayName);
+        MEETINGS.get(meetingCode).add(map);
+        Map<String, Object> response = new HashMap<>();
+        response.put("isMeetingCreator", Objects.equals(userId, 1));
+        response.put("joinedUsers", MEETINGS.get(meetingCode));
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/leave")
-    public ResponseEntity<?> leaveMeeting( @RequestParam String peerId) {
-        return meetingService.leaveMeeting(peerId);
+    public ResponseEntity<?> leaveMeeting( @RequestParam String peerId, @RequestParam String meetingCode) {
+        int idx = -1;
+        for(int i=0; i < MEETINGS.get(meetingCode).size(); i++){
+            if(peerId == MEETINGS.get(meetingCode).get(i).get("peerId")){
+                idx=i;
+                break;
+            }
+        }
+        if(idx >-1){
+            MEETINGS.get(meetingCode).remove(idx);
+        }
+        return new ResponseEntity<>("Meeting Left",HttpStatus.OK);
+    }
+    @PostMapping("/close")
+    public ResponseEntity<?> closeMeeting(@RequestParam String meetingCode) {
+        MEETINGS.remove(meetingCode);
+        return new ResponseEntity<>("Meeting Closed",HttpStatus.OK);
     }
 }
